a5eb049d6640da1a87d40415c659449739c044ca
TomoyaWaniko <alligatorfree12@gmail.com>
2023-12-08 13:34:29 -0800

    Update

---
+++ node_ztm/script1.js
@@ -0,0 +1,16 @@
+// const urls = [
+//     "https://jsonplaceholder.typicode.com/users",
+//     "https://jsonplaceholder.typicode.com/posts",
+//     "https://jsonplaceholder.typicode.com/albums"
+// ]
+//
+// Promise.all(urls.map(url => {
+//     return fetch(url)
+//         .then(people => people.json());
+// }))
+//     .then(array => {
+//         // console.log(array[0]);
+//     })
+//     .catch(err => console.log("fix this: ", err))
+//     .finally(data => console.log(data));
+
