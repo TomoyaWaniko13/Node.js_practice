35015bdca9279a5744af4d4af78ac12490b7e962
TomoyaWaniko <alligatorfree12@gmail.com>
2023-12-19 07:14:08 -0800

    Update

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+/////////////////////
+// File IO
 
 //Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -7,20 +12,50 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", textOut);
 
 //Non-Blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-    if (err){
-        console.log("Error!!")
-    }
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-        console.log(data2);
-        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-            console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//     if (err){
+//         console.log("Error!!")
+//     }
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//         console.log(data2);
+//         fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//             console.log(data3);
+//
+//             fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8",err => {
+//                 console.log("Your file has been written.")
+//             });
+//         })
+//     })
+// });
+//
+// console.log("Loading...")
 
-            fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8",err => {
-                console.log("Your file has been written.")
-            });
+///////////////////////////////////
+//Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === "/overview") {
+        res.end("This is the Overview");
+    } else if (pathName === "/product") {
+        res.end("This is the Product");
+    } else if (pathName === "/api") {
+        res.writeHead(200,{"Content-type": "application/json"});
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world"
         })
-    })
-});
+        res.end("<h1>Page not found!</h1>");
+    }
+})
 
-console.log("Loading...")
+server.listen(3000, "127.0.0.1", () => {
+    console.log("Listening to requests on port 3000");
+})
+
