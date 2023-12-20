b9b00d3870e736eee61cbb0831602d07fa7bedba
TomoyaWaniko <alligatorfree12@gmail.com>
2023-12-19 18:48:32 -0800

    Update

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,106 +1,15 @@
 const fs = require("fs");
-const http = require("http");
-const url = require("url");
 
-/////////////////////
-// File IO
+// synchronous code...
+// const input = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(input);
+// const output = `This is what we know about the avocado: ${input}`;
+// fs.writeFileSync("./txt/output.txt", output);
 
-//Blocking, synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-
-//Non-Blocking, asynchronous way
+//asynchronous code...
 // fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//     if (err){
-//         console.log("Error!!")
-//     }
+//     console.log("data1:", data1);
 //     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//         console.log(data2);
-//         fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//             console.log(data3);
-//
-//             fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8",err => {
-//                 console.log("Your file has been written.")
-//             });
-//         })
+//         console.log("data2:", data2)
 //     })
 // });
-//
-// console.log("Loading...")
-
-///////////////////////////////////
-//Server
-
-//temp:a string representing the HTML template.
-//product:an object representing a product.
-const replaceTemplate = (temp, product) => {
-    //output: It stores the result of the template string after it has been proceesed by replacing various placeholders
-    // with actual product data.
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    return output;
-};
-
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
-
-
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
-
-const server = http.createServer((req, res) => {
-
-    const {query, pathname} = url.parse(req.url, true);
-    console.log("url.parse():\n", url.parse(req.url, true));
-    console.log("query:", query);
-    console.log("pathname:", pathname);
-
-    // console.log(req.url);
-    // console.log(url.parse(req.url, true));
-    // const pathName = req.url;
-    // console.log("pathName: ",pathName);
-
-    //Overview page
-    if (pathname === "/" || pathname === "/overview") {
-        res.writeHead(200, {"Content-type": "text/html"})
-
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
-        const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
-        res.end(output);
-
-        //product page
-    } else if (pathname === "/product") {
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct, product);
-        res.end(output);
-
-        //API
-    } else if (pathname === "/api") {
-        res.writeHead(200, {"Content-type": "application/json"});
-        res.end(data);
-
-        //NOT found
-    } else {
-        res.writeHead(404, {
-            "Content-type": "text/html",
-            "my-own-header": "hello-world"
-        })
-        res.end("<h1>Page not found!</h1>");
-    }
-})
-
-server.listen(3000, "127.0.0.1", () => {
-    console.log("Listening to requests on port 3000");
-})
-
