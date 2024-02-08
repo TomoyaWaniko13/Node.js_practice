81b962bc2225e947588bae0e4ecb655bb4ccca4e
TomoyaWaniko <alligatorfree12@gmail.com>
2024-02-08 06:43:01 -0800

    Update

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+
 app.get('/', (req, res) => {
   res
     .status(200)
@@ -12,21 +18,42 @@ app.get('/', (req, res) => {
     });
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+app.post('/api/v1/tours', (req, res) => {
+
+  const newId = tours[tours.length - 1];
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+
+});
 
 app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours
-    }
-  });
+  res
+    .status(200)
+    .json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
+  console.log(tours);
 });
-
