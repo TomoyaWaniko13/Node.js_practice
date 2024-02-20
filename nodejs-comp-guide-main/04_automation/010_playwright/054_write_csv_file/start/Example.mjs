f0343667d44e8df4114dafa1aa26c53199d2f5ec
TomoyaWaniko <alligatorfree12@gmail.com>
2024-02-19 19:55:43 -0800

    Update

--- nodejs-comp-guide-main/04_automation/010_playwright/054_write_csv_file/start/Example.mjs
+++ nodejs-comp-guide-main/04_automation/010_playwright/054_write_csv_file/start/Example.mjs
@@ -1,5 +1,28 @@
 import {chromium} from "@playwright/test";
+import * as fs from "fs";
+import {Parser} from "json2csv";
 
 (async () => {
+    const browser = await chromium.launch({headless: false, slowMo: 500})
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+
+    const cardsLocator = page.locator('.cards.list-group-item');
+    const numberOfCards = await cardsLocator.count();
+
+    const cardsTexts = [];
+
+    for (let i = 0; i < numberOfCards; i++) {
+        const eachCardLocator = cardsLocator.locator(`nth=${i}`);
+        const eachCardText = await eachCardLocator.textContent();
+        cardsTexts.push({
+            name: eachCardText
+        })
+    }
+    await browser.close();
+
+    const parser = new Parser();
+    const csv = parser.parse(cardsTexts);
+    fs.writeFileSync("./text-data.csv", csv);
 
 })();
