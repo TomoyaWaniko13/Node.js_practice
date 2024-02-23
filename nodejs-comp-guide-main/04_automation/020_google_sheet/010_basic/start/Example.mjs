2e01b3587c1b33d1e4da012e6217baa606dde706
TomoyaWaniko <alligatorfree12@gmail.com>
2024-02-22 16:33:13 -0800

    Update

---
+++ nodejs-comp-guide-main/04_automation/020_google_sheet/010_basic/start/Example.mjs
@@ -0,0 +1,9 @@
+import {GoogleSpreadsheet} from "google-spreadsheet";
+import env from 'dotenv';
+env.config();
+
+    // https://docs.google.com/spreadsheets/d/10xihAYhtdWP5xZObOcqty3_ajzI1dJJoot46A2tVnTY/edit#gid=0
+
+    (async () => {
+        new GoogleSpreadsheet()
+    });
