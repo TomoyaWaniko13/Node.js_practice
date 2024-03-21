22fdcdf7c98f458a761ca0d5347e941a12a02b1c
TomoyaWaniko <alligatorfree12@gmail.com>
2024-03-22 08:53:28 +0900

    Update

--- nodejs-comp-guide-main/06_backend/070_express_basic/start/app.mjs
+++ nodejs-comp-guide-main/06_backend/070_express_basic/start/app.mjs
@@ -1,24 +1,26 @@
-import express from 'express';
+import express from "express";
 
-const PORT = 8000;
+const PORT = 11000;
 const app = express();
 
 app.get('/', function (req, res) {
     res.send(`
-        <a href="/result?param1=1&param2=2">GET method link</a>
-        <form action="/result" method="POST">
-            <input type="text" name="title">
-            <input type="text" name="description">
-            <input type="submit">
-        </form>
-    `)
+    <a href="/result?param1=1&param2=2">Get method link</a>
+    <form action="/result" method="POST">
+        <input type="text" name="title">   
+        <input type="text" name="description">
+        <input type="submit">
+    </form>
+    `);
 });
 
 app.get('/result', function (req, res) {
+    //obtain parameters of a GET method.
     const params = req.query;
     console.log(params);
 });
 
 app.listen(PORT, function () {
-    console.log(`Server starts at port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
