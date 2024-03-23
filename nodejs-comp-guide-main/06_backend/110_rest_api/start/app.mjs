91b9fdfbab90a1374d9182619f923b8c597d33ce
TomoyaWaniko <alligatorfree12@gmail.com>
2024-03-24 08:24:55 +0900

    Update

--- nodejs-comp-guide-main/06_backend/110_rest_api/start/app.mjs
+++ nodejs-comp-guide-main/06_backend/110_rest_api/start/app.mjs
@@ -1,51 +1,12 @@
 import express from "express";
+import apiRoutes from "./api-routes/index.mjs";
 
 const PORT = 8080;
 const app = express();
 
 app.use(express.json());
 
-const products = [
-    {name: 'table', price: 1000},
-    {name: 'chair', price: 200},
-    {name: 'clock', price: 700},
-];
-
-app.get('/products', function (req, res) {
-    res.json(products);
-});
-
-app.get('/products/:id', function (req, res) {
-    const targetId = req.params.id;
-    const targetProduct = products[targetId];
-    res.json(targetProduct);
-});
-
-app.post('/products', function (req, res) {
-    const newProduct = req.body;
-    products.push(newProduct);
-    res.json(newProduct);
-});
-
-app.delete('/products/:id', function (req, res) {
-    const targetId = req.params.id;
-    products.splice(targetId, 1);
-    res.json({targetId});
-});
-
-app.patch('/products/:id', function (req, res) {
-    const targetProduct = products[req.params.id];
-
-    if (req.body.hasOwnProperty('name')) {
-        targetProduct.name = req.body.name;
-    }
-
-    if (req.body.hasOwnProperty('price')) {
-        targetProduct.price = req.body.price;
-    }
-
-    res.json(targetProduct);
-});
+app.use('/api', apiRoutes);
 
 app.listen(PORT, function () {
     console.log(`server is running at http://localhost:${PORT}`);
