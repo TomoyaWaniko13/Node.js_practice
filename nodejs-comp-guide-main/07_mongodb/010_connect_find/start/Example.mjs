ebb7279425a780c35a44f655871923d2c183b8c2
TomoyaWaniko <alligatorfree12@gmail.com>
2024-03-26 13:21:59 +0900

    Update

--- nodejs-comp-guide-main/07_mongodb/010_connect_find/start/Example.mjs
+++ nodejs-comp-guide-main/07_mongodb/010_connect_find/start/Example.mjs
@@ -1,13 +1,11 @@
-import {MongoClient, ObjectID, ServerApiVersion} from "mongodb";
-import env from 'dotenv';
+import {MongoClient, ServerApiVersion} from "mongodb";
+import env from "dotenv";
 
 env.config({path: '../../.env'});
 
 
-const uri = process.env.MONGO_URI;
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGO_URI, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
@@ -21,23 +19,36 @@ async function getCollection() {
         const db = client.db('bookshelf');
         return db.collection('books');
     } catch {
-        await client.close();
+        client.close();
     }
 }
 
 async function getAllBooks() {
-    const collection = await getCollection();
+    const col = await getCollection();
 
-    // let cursor = collection.find({$or: [{rating: 3}, {title: 'バックエンド開発'}]});
-    // cursor = collection.find({title: {$in: ['ごんぎつね４', 'バックエンド開発']}});
-    // let cursor = collection.find({rating: {$gt: 2, $lt: 5}});
-    // let cursor = collection.find({rating: {$gte: 2, $lte: 5}});
-    // let cursor = collection.find({rating: {$gte: 2, $lte: 4}}).sort({rating: 1});
-    // let cursor = collection.find({rating: {$gte: 2, $lte: 4}}).sort({rating: -1});
-    let cursor = collection.find({title: {$regex: /^潮/}});
+    // let cursor = col.find();
+    // let cursor = col.find({title: '潮騒'});
+    // let cursor = col.find({rating: 5});
+    // let cursor = col.find({title: '潮騒', rating: 5});
+    // let cursor = col.find({$and: [{rating: 5}, {title: '潮騒'}]});
+    // let cursor = col.find({$or: [{rating: 3}, {title: 'バックエンド開発'}]});
+    // let cursor = col.find({title: {$in: ['ごんぎつね4', 'バックエンド開発']}});
+    // let cursor = col.find({rating: {$gt: 3, $lt: 5}});
+    // let cursor = col.find({rating: {$gte: 3, $lte: 5}}).sort({rating: 1});
+
+    // let cursor = col.find();
+    // let cursor = col.find({title: '潮騒'});
+    // let cursor = col.find({rating: 5});
+    // let cursor = col.find({title: '潮騒', rating: 5});
+    // let cursor = col.find({$or: [{rating: 3, title: 'バックエンド開発'}]});
+    // let cursor = col.find({title: {$in: ['ごんぎつね4', 'バックエンド開発']}});
+    // let cursor = col.find({rating: {$gt: 2, $lt: 4}});
+    let cursor = col.find({rating: {$gte: 2, $lte: 4}}).sort({rating: -1});
+
 
     const result = await cursor.toArray();
     console.log(result);
+
     await client.close();
 }
 
