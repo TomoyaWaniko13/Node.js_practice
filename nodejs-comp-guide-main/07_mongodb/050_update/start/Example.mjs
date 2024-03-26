a1c4c637aadd3f13f358364737ef494026d162c0
TomoyaWaniko <alligatorfree12@gmail.com>
2024-03-26 11:08:43 +0900

    Update

--- nodejs-comp-guide-main/07_mongodb/050_update/start/Example.mjs
+++ nodejs-comp-guide-main/07_mongodb/050_update/start/Example.mjs
@@ -1,22 +1,31 @@
 import env from 'dotenv';
-env.config();
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
-import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+env.config({path: '../../.env'});
+
+
+const client = new MongoClient(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
 
 async function getCollection() {
-  try {
-    await client.connect();
-    const db = client.db('bookshelf');
-    return db.collection('books');
-  } catch {
-    await client.close();
-  }
+    try {
+        await client.connect();
+        const db = client.db('bookshelf');
+        return db.collection('books');
+    } catch {
+        await client.close();
+    }
 }
 
 updateBook();
-async function updateBook() {
-  const col = await getCollection();
 
-  await client.close();
+async function updateBook() {
+    const col = await getCollection();
+    // await col.updateOne({title: 'バックエンド開発'}, {$set: {rating: 3}});
+    await col.updateMany({description: '三島由紀夫'}, {$set: {rating: 5}});
+
+    await client.close();
 }
